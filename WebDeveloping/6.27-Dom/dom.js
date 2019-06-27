fcdbee75c70af2a1a82077372c2c7986fe83d1b5
Dai <shihengdaicoding@gmail.com>
2019-06-28 11:34:17 +1200

    nd an empty message aborts the commit.
    w On branch master
    this is a test# Changes to be committed:

---
+++ WebDeveloping/6.27-Dom/dom.js
@@ -0,0 +1,51 @@
+
+let form = document.getElementById('addForm');
+let itemList = document.getElementById('items');
+let del = document.getElementsByClassName('btn btn-danger btn-sm float-right delete');
+
+form.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+filter.addEventListener('keyup', filterItems);
+
+function addItem(e) {
+    e.preventDefault();
+    let newItem = document.getElementById('item').value;
+    let li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.appendChild(document.createTextNode(newItem));
+
+    let deleteBtn = document.createElement('button');
+
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    deleteBtn.appendChild(document.createTextNode('X'));
+    li.appendChild(deleteBtn);
+
+    itemList.appendChild(li);
+
+}
+
+function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are you Sure?')) {
+            let li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+
+}
+
+//Filter Items
+function filterItems(e) {
+    let text = e.target.value.toLowerCase();
+    let items = itemList.getElementsByTagName('li');
+    Array.from(items).forEach(function (item) {
+        let itemName = item.firstChild.textContent;
+
+        if (itemName.toLowerCase().indexOf(text) != -1) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+
+}
